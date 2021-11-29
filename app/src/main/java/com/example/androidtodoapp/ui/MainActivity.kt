b07588ec31a6d305292f007504c1224b56e7b915
matthewogtong <ogtongm@gmail.com>
2021-11-29 18:49:36 -0500

    Add NavHostFragment to Main Activity

--- app/src/main/java/com/example/androidtodoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidtodoapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androidtodoapp
+package com.example.androidtodoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidtodoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
