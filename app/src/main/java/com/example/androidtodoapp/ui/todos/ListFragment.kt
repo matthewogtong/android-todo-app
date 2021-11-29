1c695881384226b9792494d58a97b67ede7a71ce
matthewogtong <ogtongm@gmail.com>
2021-11-29 17:44:50 -0500

    Add ListFragment class

---
+++ app/src/main/java/com/example/androidtodoapp/ui/todos/ListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.androidtodoapp.ui.todos
+
+import androidx.fragment.app.Fragment
+import com.example.androidtodoapp.R
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+}
