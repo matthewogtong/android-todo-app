dfb26372d260d74d932e2bcf35de36e7997ad1f2
matthewogtong <ogtongm@gmail.com>
2021-11-30 13:24:17 -0500

    Adjust DaoQueries

--- app/src/main/java/com/example/androidtodoapp/data/TodoDao.kt
+++ app/src/main/java/com/example/androidtodoapp/data/TodoDao.kt
@@ -1,13 +1,23 @@
 package com.example.androidtodoapp.data
 
 import androidx.room.*
+import com.example.androidtodoapp.ui.todos.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
 
-    @Query("SELECT * FROM todo_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTodos(searchQuery: String) : Flow<List<Todo>>
+    fun getTodos(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder) {
+            SortOrder.BY_DATE -> getTodosSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTodosSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM todo_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTodosSortedByName(searchQuery: String, hideCompleted: Boolean) : Flow<List<Todo>>
+
+    @Query("SELECT * FROM todo_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTodosSortedByDateCreated(searchQuery: String, hideCompleted: Boolean) : Flow<List<Todo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(todo : Todo)
--- app/src/main/java/com/example/androidtodoapp/ui/todos/TodoViewModel.kt
+++ app/src/main/java/com/example/androidtodoapp/ui/todos/TodoViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.asLiveData
 import com.example.androidtodoapp.data.TodoDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
@@ -15,10 +16,21 @@ class TodoViewModel @Inject constructor(
 
     val searchQuery = MutableStateFlow("")
 
-    private val todosFlow = searchQuery.flatMapLatest {
-        todoDao.getTodos(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
+    private val todosFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        todoDao.getTodos(query, sortOrder, hideCompleted)
     }
 
     val todos = todosFlow.asLiveData()
 
 }
+
+enum class SortOrder { BY_NAME, BY_DATE}
