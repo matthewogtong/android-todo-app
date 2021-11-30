0df4eaaa409697f600248b6d7b819bb569bc79b0
matthewogtong <ogtongm@gmail.com>
2021-11-29 21:50:53 -0500

    Add DiffCallback class to use as an argument for ListAdapter

--- app/src/main/java/com/example/androidtodoapp/ui/todos/TodosAdapter.kt
+++ app/src/main/java/com/example/androidtodoapp/ui/todos/TodosAdapter.kt
@@ -1,21 +1,25 @@
 package com.example.androidtodoapp.ui.todos
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidtodoapp.data.Todo
 import com.example.androidtodoapp.databinding.ItemTodoBinding
 
 
-class TodosAdapter : ListAdapter<Todo, TodosAdapter.TodoViewHolder>() {
+class TodosAdapter : ListAdapter<Todo, TodosAdapter.TodoViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
     }
 
     class TodoViewHolder(private val binding : ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -29,6 +33,14 @@ class TodosAdapter : ListAdapter<Todo, TodosAdapter.TodoViewHolder>() {
             }
         }
 
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Todo>() {
+        override fun areItemsTheSame(oldItem: Todo, newItem: Todo) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Todo, newItem: Todo) =
+            oldItem == newItem
     }
 
 }
