b2220f522c58e59128f4c40953d7a607327f128b
matthewogtong <ogtongm@gmail.com>
2021-12-01 09:09:42 -0500

    Add catch statement incase of IOException and set values to default with emptyPreferences

--- app/src/main/java/com/example/androidtodoapp/data/PreferencesManager.kt
+++ app/src/main/java/com/example/androidtodoapp/data/PreferencesManager.kt
@@ -3,13 +3,18 @@ package com.example.androidtodoapp.data
 
 import android.content.Context
 import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.emptyPreferences
 import androidx.datastore.preferences.preferencesKey
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 enum class SortOrder { BY_NAME, BY_DATE}
+
+data class FilterPreferences(val sortOrder: SortOrder, val hideCompleted: Boolean)
 
 @Singleton
 class PreferencesManager @Inject constructor(@ApplicationContext context: Context) {
@@ -17,10 +22,24 @@ class PreferencesManager @Inject constructor(@ApplicationContext context: Contex
     private val dataStore = context.createDataStore("user_preferences")
 
     val preferencesFlow = dataStore.data
+
+        .catch { exception ->
+            if (exception is IOException) {
+
+                emit(emptyPreferences())
+            }
+
+        }
+
         .map { preferences ->
+
             val sortOrder = SortOrder.valueOf(
                 preferences[PreferencesKeys.SORT_ORDER] ?: SortOrder.BY_DATE.name
             )
+
+            val hideCompleted = preferences[PreferencesKeys.HIDE_COMPLETED] ?: false
+            FilterPreferences(sortOrder, hideCompleted)
+
         }
 
     private object PreferencesKeys {
