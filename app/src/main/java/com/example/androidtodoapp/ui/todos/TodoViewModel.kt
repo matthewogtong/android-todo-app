f8d3722269538601fe03fd789502e984f964fd62
matthewogtong <ogtongm@gmail.com>
2021-12-01 10:38:07 -0500

    Add suspending functions for options resource to TodoViewModel

--- app/src/main/java/com/example/androidtodoapp/ui/todos/TodoViewModel.kt
+++ app/src/main/java/com/example/androidtodoapp/ui/todos/TodoViewModel.kt
@@ -2,32 +2,42 @@ package com.example.androidtodoapp.ui.todos
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.androidtodoapp.data.PreferencesManager
 import com.example.androidtodoapp.data.SortOrder
 import com.example.androidtodoapp.data.TodoDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TodoViewModel @Inject constructor(
-    private val todoDao : TodoDao
+    private val todoDao : TodoDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
 
-    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
-    val hideCompleted = MutableStateFlow(false)
+    val preferencesFlow = preferencesManager.preferencesFlow
 
     private val todosFlow = combine(
         searchQuery,
-        sortOrder,
-        hideCompleted
-    ) { query, sortOrder, hideCompleted ->
-        Triple(query, sortOrder, hideCompleted)
-    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
-        todoDao.getTodos(query, sortOrder, hideCompleted)
+        preferencesFlow
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        todoDao.getTodos(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
+    }
+
+    fun onSortOrderSelected(sortOrder : SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompleted(hideCompleted : Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
     }
 
     val todos = todosFlow.asLiveData()
